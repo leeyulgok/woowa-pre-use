c0e01d3d0f57429a1201f35f3b23eaf2481af75b
leeyulgok <dbfrhr20@gmail.com>
2023-10-24 17:28:13 +0900

    Feat: 게임시작 상태 설정

--- components/[weeks]/weekOne.tsx
+++ components/[weeks]/weekOne.tsx
@@ -1,10 +1,12 @@
-import { FC } from "react";
+import React, { FC, useState } from "react";
 
 type WeekOneProps = {
-  title: string,
-}
+  title: string;
+};
 
-const WeekOne: FC<WeekOneProps> = ({title}) => {
+const WeekOne: FC<WeekOneProps> = ({ title }) => {
+  const [isGameStarted, setGameStarted] = useState(false);
+
   return (
     <main className="WeekOneContainer p-3 w-full h-full">
       <div className="headerContainer">
@@ -12,23 +14,34 @@ const WeekOne: FC<WeekOneProps> = ({title}) => {
         <h2>숫자 야구 게임</h2>
       </div>
       <section className="contentContainer w-full h-full">
-        <div className="baseballBox bg-gray-500 flex border border-black w-full h-1/2">
-          <div className="answerBox bg-white w-10/12 flex p-2 justify-center items-center">
-            <div className="answerOne border border-black rounded-full w-52 h-52 m-4" />
-            <div className="answerTwo border border-black rounded-full w-52 h-52 m-4" />
-            <div className="answerThree border border-black rounded-full w-52 h-52 m-4" />
-          </div>
-          <div className="inputBox w-2/6 p-2">
-            <h3>숫자를 입력해주세요</h3>
-            <form action="" className="flex flex-col">
-              <input type="text" name="userNumber" id="userNumber"/>
-              <button className="border border-black bg-white">확인</button>
-            </form>
-          </div>
+        <div className="baseballBox bg-gray-500 flex border border-black w-full h-1/2 justify-center items-center">
+          {!isGameStarted ? (
+            <button
+              className="border border-black bg-white p-4"
+              onClick={() => setGameStarted(true)}
+            >
+              게임시작
+            </button>
+          ) : (
+            <>
+              <div className="answerBox bg-white w-10/12 flex p-2 justify-center items-center">
+                <div className="answerOne border border-black rounded-full w-52 h-52 m-4" />
+                <div className="answerTwo border border-black rounded-full w-52 h-52 m-4" />
+                <div className="answerThree border border-black rounded-full w-52 h-52 m-4" />
+              </div>
+              <div className="inputBox w-2/6 p-2">
+                <h3>숫자를 입력해주세요</h3>
+                <form action="" className="flex flex-col">
+                  <input type="text" name="userNumber" id="userNumber" />
+                  <button className="border border-black bg-white">확인</button>
+                </form>
+              </div>
+            </>
+          )}
         </div>
       </section>
     </main>
-  )    
-}
+  );
+};
 
 export default WeekOne;
