cf79efdcf36791cb9b927ef3bfa0ae808b100a25
leeyulgok <dbfrhr20@gmail.com>
2023-11-15 15:49:17 +0900

    feat: 날짜 예약 기능 추가

--- components/[weeks]/WeekFour.tsx
+++ components/[weeks]/WeekFour.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState } from "react";
 import dynamic from "next/dynamic";
-import 'react-calendar/dist/Calendar.css';
+import "react-calendar/dist/Calendar.css";
 
 type WeekFourProps = {
   title: string;
@@ -15,12 +15,19 @@ const Calendar = dynamic(() => import("react-calendar"), { ssr: false });
 
 const WeekFour: FC<WeekFourProps> = ({ title }) => {
   const [value, setValue] = useState<Date>(new Date(2023, 11, 1));
-  
+  const [isReservation, setIsReservation] = useState<boolean>(false);
+
   const tileDisabled = ({ date, view }: tileDisabledProps) => {
-    if (view === 'month' && (date.getFullYear() !== 2023 || date.getMonth() !== 11)) {
+    if (isReservation) return true;
+
+    if (view === "month" && (date.getFullYear() !== 2023 || date.getMonth() !== 11)) {
       return true;
     }
     return false;
+  };
+
+  const checkReservation = () => {
+    setIsReservation(true);
   };
 
   return (
@@ -35,9 +42,29 @@ const WeekFour: FC<WeekFourProps> = ({ title }) => {
             value={value}
             tileDisabled={tileDisabled}
           />
-          <button className="calendarButton border border-black cursor-pointer w-full">확정</button>
+          <button
+            className="calendarButton border border-black w-full"
+            onClick={checkReservation}
+            disabled={isReservation}
+          >
+            확정
+          </button>
         </div>
-        <div className="menuContainer"></div>
+        {isReservation ? (
+          <div className="menuContainer items-center justify-center border border-black w-full h-full m-1">
+            <div className="menuHeader">
+              <h1>메뉴판</h1>
+            </div>
+            <div className="menuListBox flex">
+              <div className="appetizers border border-black w-full h-full m-1"></div>
+              <div className="mains border border-black w-full h-full m-1"></div>
+              <div className="desserts border border-black w-full h-full m-1"></div>
+              <div className="drinks border border-black w-full h-full m-1"></div>
+            </div>
+          </div>
+        ) : (
+          <div></div>
+        )}
         <div className="resultContainer"></div>
       </div>
     </main>
