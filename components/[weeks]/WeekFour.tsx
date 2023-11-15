03ea13400c00cf7bb4bb49ddc41a7edcf9f03f9a
leeyulgok <dbfrhr20@gmail.com>
2023-11-15 16:18:37 +0900

    feat: 메뉴판 작성

--- components/[weeks]/WeekFour.tsx
+++ components/[weeks]/WeekFour.tsx
@@ -1,5 +1,7 @@
 import React, { FC, useState } from "react";
 import dynamic from "next/dynamic";
+import MENU_LIST from "../utils/menuList";
+
 import "react-calendar/dist/Calendar.css";
 
 type WeekFourProps = {
@@ -20,7 +22,10 @@ const WeekFour: FC<WeekFourProps> = ({ title }) => {
   const tileDisabled = ({ date, view }: tileDisabledProps) => {
     if (isReservation) return true;
 
-    if (view === "month" && (date.getFullYear() !== 2023 || date.getMonth() !== 11)) {
+    if (
+      view === "month" &&
+      (date.getFullYear() !== 2023 || date.getMonth() !== 11)
+    ) {
       return true;
     }
     return false;
@@ -28,6 +33,10 @@ const WeekFour: FC<WeekFourProps> = ({ title }) => {
 
   const checkReservation = () => {
     setIsReservation(true);
+  };
+
+  const getMenuItemsByCategory = (category: string) => {
+    return Object.values(MENU_LIST).filter((item) => item.category === category);
   };
 
   return (
@@ -56,10 +65,105 @@ const WeekFour: FC<WeekFourProps> = ({ title }) => {
               <h1>메뉴판</h1>
             </div>
             <div className="menuListBox flex">
-              <div className="appetizers border border-black w-full h-full m-1"></div>
-              <div className="mains border border-black w-full h-full m-1"></div>
-              <div className="desserts border border-black w-full h-full m-1"></div>
-              <div className="drinks border border-black w-full h-full m-1"></div>
+              <div className="appetizers border border-black w-full h-full m-1 p-2">
+                <div className="categoryTitle m-2">
+                  <h3>에피타이저</h3>
+                </div>
+                {getMenuItemsByCategory("appetizers").map((item) => (
+                  <div
+                    className="foods border border-black p-2 m-2"
+                    key={item.name}
+                  >
+                    <div className="name">{item.name}</div>
+                    <div className="price">{item.price}</div>
+                    <div className="countBox">
+                      <label htmlFor={item.name}>개수</label>
+                      <input
+                        type="number"
+                        className="border border-black m-1"
+                        name={item.name}
+                        min={0}
+                        max={20}
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="mains border border-black w-full h-full m-1">
+              <div className="categoryTitle m-2">
+                  <h3>메인</h3>
+                </div>
+                {getMenuItemsByCategory("mains").map((item) => (
+                  <div
+                    className="foods border border-black p-2 m-2"
+                    key={item.name}
+                  >
+                    <div className="name">{item.name}</div>
+                    <div className="price">{item.price}</div>
+                    <div className="countBox">
+                      <label htmlFor={item.name}>개수</label>
+                      <input
+                        type="number"
+                        className="border border-black m-1"
+                        name={item.name}
+                        min={0}
+                        max={20}
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="desserts border border-black w-full h-full m-1">
+              <div className="categoryTitle m-2">
+                  <h3>디저트</h3>
+                </div>
+                {getMenuItemsByCategory("desserts").map((item) => (
+                  <div
+                    className="foods border border-black p-2 m-2"
+                    key={item.name}
+                  >
+                    <div className="name">{item.name}</div>
+                    <div className="price">{item.price}</div>
+                    <div className="countBox">
+                      <label htmlFor={item.name}>개수</label>
+                      <input
+                        type="number"
+                        className="border border-black m-1"
+                        name={item.name}
+                        min={0}
+                        max={20}
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="drinks border border-black w-full h-full m-1">
+              <div className="categoryTitle m-2">
+                  <h3>음료</h3>
+                </div>
+                {getMenuItemsByCategory("drinks").map((item) => (
+                  <div
+                    className="foods border border-black p-2 m-2"
+                    key={item.name}
+                  >
+                    <div className="name">{item.name}</div>
+                    <div className="price">{item.price}</div>
+                    <div className="countBox">
+                      <label htmlFor={item.name}>개수</label>
+                      <input
+                        type="number"
+                        className="border border-black m-1"
+                        name={item.name}
+                        min={0}
+                        max={20}
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="orderBox">
+                <button className="orderButton border border-black m-1 p-1">주문하기</button>
             </div>
           </div>
         ) : (
